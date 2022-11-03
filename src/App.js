cb59137c9dfe301e4d2d453ccdd23e243e4a69d3
Phorez <dev.phorez@gmail.com>
2022-11-03 11:22:04 +0330

    Add Services using aos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import React from 'react'
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Components/Home/Home';
+import Services from "./Components/Services/Services";
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function App() {
+	
+	React.useEffect(() => {
+		AOS.init(
+			{
+				delay: 300,
+				once: true,
+				easing: 'ease-in-sine',
+			}
+		);
+	}, [])
+	
     return (
 	    <>
 		    <Navbar />
 		    <Home />
+		    <Services />
 	    </>
     );
 }
---
+++ src/Components/Services/Services.js
@@ -0,0 +1,84 @@
+import './Services.css'
+
+
+function Services () {
+	
+	return (
+		<section className='position-relative' style={ { paddingBottom: 120 } }>
+			<div className="container">
+				<div className="row justify-content-center text-center">
+					<div className='col-xl-5 col-md-10 col-lg-8 services-title'>
+						<p className="text-primary">
+							OUR SERVICES
+						</p>
+						<h2>
+							Provide Awesome Service With Our Tools
+						</h2>
+					</div>
+				</div>
+				<div className="row">
+					<div className='col-xl-3 col-lg-4 col-md-6 col-sm-6' data-aos='fade-right'>
+						<div className="single-card">
+							<h5 style={ { fontWeight: 700 } }>
+								Discover, Explore the Product
+							</h5>
+							<div className='service-icon' style={ { width: 80, height: 80 } }>
+								<img style={ { width: 45 } } src="/Images/services1.svg" alt="service 1"/>
+							</div>
+							<p>
+								Effective strategies to help you reach customers.
+							</p>
+						</div>
+					</div>
+					<div className='col-xl-3 col-lg-4 col-md-6 col-sm-6' data-aos='fade-up'>
+						<div className="single-card">
+							<h5 style={ { fontWeight: 700 } }>
+								Discover, Explore the Product
+							</h5>
+							<div className='service-icon' style={ { width: 80, height: 80 } }>
+								<img style={ { width: 45 } } src="/Images/services2.svg" alt="service 1"/>
+							</div>
+							<p>
+								Effective strategies to help you reach customers.
+							</p>
+						</div>
+					</div>
+					<div className='col-xl-3 col-lg-4 col-md-6 col-sm-6' data-aos='fade-up'>
+						<div className="single-card">
+							<h5 style={ { fontWeight: 700 } }>
+								Discover, Explore the Product
+							</h5>
+							<div className='service-icon' style={ { width: 80, height: 80 } }>
+								<img style={ { width: 45 } } src="/Images/services3.svg" alt="service 1"/>
+							</div>
+							<p>
+								Effective strategies to help you reach customers.
+							</p>
+						</div>
+					</div>
+					<div className='col-xl-3 col-lg-4 col-md-6 col-sm-6' data-aos='fade-left'>
+						<div className="single-card">
+							<h5 style={ { fontWeight: 700 } }>
+								Discover, Explore the Product
+							</h5>
+							<div className='service-icon' style={ { width: 80, height: 80 } }>
+								<img style={ { width: 45 } } src="/Images/services3.svg" alt="service 1"/>
+							</div>
+							<p>
+								Effective strategies to help you reach customers.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			{/* Todo fixed z-index shape 2 bug and add shape 2 */}
+			{/*<div className="shape-1 d-none d-xl-block">*/}
+			{/*</div>*/}
+			{/*<div className="shape-2">*/}
+			{/*	<img src="/Images/shape-2.webp" alt="shape 2" className=''/>*/}
+			{/*</div>*/}
+		</section>
+	)
+}
+
+export default Services
