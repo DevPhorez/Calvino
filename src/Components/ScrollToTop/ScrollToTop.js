58cb6e013c47fd57162cc972c452db9541f1a508
Phorez <dev.phorez@gmail.com>
2022-11-15 22:09:21 +0330

    update scroll to top (visible & unvisible)

--- src/Components/ScrollToTop/ScrollToTop.js
+++ src/Components/ScrollToTop/ScrollToTop.js
@@ -1,3 +1,5 @@
+import * as React from 'react'
+
 import './ScrollToTop.css'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -6,7 +8,27 @@ import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
 import Scroll from 'react-scroll'
 
 function ScrollToTop () {
-	const  scroll = Scroll.animateScroll;
+	
+	const [isVisible, setIsVisible] = React.useState(false)
+	
+	const button = React.useRef()
+	
+	function ToggleVisible () {
+		const scrolled = document.documentElement.scrollTop
+		
+		
+		if (scrolled > 300) {
+			setIsVisible(true)
+			button.current.classList.add('scroll-to-top-animation')
+			
+		} else if (scrolled <= 300) {
+			setIsVisible(false)
+		}
+	}
+	
+	window.addEventListener('scroll', ToggleVisible)
+	
+	const scroll = Scroll.animateScroll;
 	
 	function BackToTop () {
 		scroll.scrollTo(0, 0)
@@ -15,7 +37,7 @@ function ScrollToTop () {
 	
 	
 	return (
-		<div className='button-parent scroll-to-top circle'>
+		<div ref={button} className={`button-parent scroll-to-top circle`} style={ { display: isVisible ? 'inline' : 'none' } }>
 			<button className='button' onClick={ BackToTop }><a className='d-flex align-items-center justify-content-center'><FontAwesomeIcon fontSize={24} icon={faArrowUp} /></a></button>
 		</div>
 	)
