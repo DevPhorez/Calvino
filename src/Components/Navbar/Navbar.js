6d9a413a2e3f15b24509fe181a1b01b3fc97b0e8
Phorez <dev.phorez@gmail.com>
2022-11-16 10:37:06 +0330

    update navbar (bakcground color transparent & white)

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,8 +1,32 @@
 import './Navbar.css'
+import * as React from "react";
 
 function Navbar () {
+	
+	// true means navbar has background color
+	const [status, setStatus] = React.useState(false)
+	
+	const navbar = React.useRef()
+	
+	function ToggleStatus () {
+		const scrolled = document.documentElement.scrollTop
+		
+		
+		if (scrolled > 500) {
+			setStatus(true)
+			navbar.current.classList.add('fixed-top')
+			navbar.current.classList.add('navbar-animation')
+			
+		} else if (scrolled <= 500) {
+			setStatus(false)
+		}
+	}
+	
+	window.addEventListener('scroll', ToggleStatus)
+	
 	return (
-		<nav className="navbar navbar-expand-lg bg-transparent px-4 py-3 fixed-top">
+		// <nav className="navbar navbar-expand-lg bg-transparent px-4 py-3 fixed-top">
+		<nav ref={navbar} className={`${ !status ? 'position-absolute w-100 bg-transparent' : 'shadow' } navbar navbar-expand-lg px-4 py-3`}>
 			<div className="container-fluid">
 				<span className="navbar-brand pointer">
 					<img src="/Images/logo.png" alt=""/>
@@ -17,27 +41,27 @@ function Navbar () {
 					<ul className="navbar-nav header-items me-auto mb-2 mb-lg-0">
 						<li className="nav-item">
 							<a className="nav-link" aria-current="page" href="#">Home</a>
-							<div className='active'></div>
+							<div className={`active ${ status && 'mb--25' }`}></div>
 						</li>
 						<li className="nav-item">
 							<a className="nav-link" aria-current="page" href="#">Services</a>
-							<div></div>
+							<div className={ status && 'mb--25' }></div>
 						</li>
 						<li className="nav-item">
 							<a className="nav-link" aria-current="page" href="#">About</a>
-							<div></div>
+							<div className={ status && 'mb--25' }></div>
 						</li>
 						<li className="nav-item">
 							<a className="nav-link" aria-current="page" href="#">Case Study</a>
-							<div></div>
+							<div className={ status && 'mb--25' }></div>
 						</li>
 						<li className="nav-item">
 							<a className="nav-link" aria-current="page" href="#">Blog</a>
-							<div></div>
+							<div className={ status && 'mb--25' }></div>
 						</li>
 						<li className="nav-item">
 							<a className="nav-link" aria-current="page" href="#">Contact</a>
-							<div></div>
+							<div className={ status && 'mb--25' }></div>
 						</li>
 					</ul>
 					<ul className="navbar-nav header-items me-auto mb-2 mb-lg-0">
